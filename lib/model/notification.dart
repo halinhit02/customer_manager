9e1775388d00dbc73cc40c008daf64b2efa4d776
KaN1011IT <Vankhoi06102002@gmail.com>
2023-05-25 22:40:34 +0700

    kn

--- lib/model/notification.dart
+++ lib/model/notification.dart
@@ -11,12 +11,14 @@ class Notification {
     required this.createdAt,
   });
 
+
   factory Notification.fromMap(Map<dynamic, dynamic> map) => Notification(
         id: map['id'],
         title: map['title'],
         content: map['content'],
         createdAt: map['createdAt'],
       );
+
 
   Map<String, dynamic> toMap() => {
         'id': id,
